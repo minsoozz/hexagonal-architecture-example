c44e7751a29d6a5986a38fdf315bf472c2447a64
minsoo <minsoo@zuminternet.com>
2023-07-27 10:04:15 +0900

    docs: 주석 삭제

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("com.h2database:h2") // h2 : implementation
+    implementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
